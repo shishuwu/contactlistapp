7ace402acda527c4c89ce05aafc0b6939453a63d
unknown <shishu@SHISHU5.auth.hpicorp.net>
2015-09-14 21:20:10 +0800

    Angular: Add controller

---
+++ public/controllers/controller.js
@@ -0,0 +1,10 @@
+//function AppCtrl(){
+//    console.log("Hello world from controller.");
+//}
+
+
+var app = angular.module('app', []);
+
+app.controller('AppCtrl', function ($scope) {
+    console.log("Hello world from controller.");
+});
