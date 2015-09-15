96db604fab7fea8ea970a82c7497354424896299
Jason Shi <shuwu.shi@gmail.com>
2015-09-16 00:06:25 +0800

    AngularJS: controller data to view

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -7,4 +7,25 @@ var app = angular.module('app', []);
 
 app.controller('AppCtrl', function ($scope) {
     console.log("Hello world from controller.");
+    
+    person1 = {
+        name: 'Tim',
+        email: 'tim@email.com',
+        number: '(111) 111-1111'
+    };
+    
+     person2 = {
+        name: 'Emily',
+        email: 'Emily@email2.com',
+        number: '(222) 222-2222'
+    };
+    
+     person3 = {
+        name: 'John',
+        email: 'john@email3.com',
+        number: '(333) 333-3333'
+    };
+    
+    var contactlist = [person1,person2,person3];
+    $scope.contactlist = contactlist;
 });
