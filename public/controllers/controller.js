50ec62e139913e0ddd9e2ac2204387f6fb5dd4a6
Jason Shi <shuwu.shi@gmail.com>
2015-09-16 00:19:21 +0800

    AngularJS: use .get to request data from server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -5,27 +5,13 @@
 
 var app = angular.module('app', []);
 
-app.controller('AppCtrl', function ($scope) {
+app.controller('AppCtrl', function ($scope, $http) {
     console.log("Hello world from controller.");
     
-    person1 = {
-        name: 'Tim',
-        email: 'tim@email.com',
-        number: '(111) 111-1111'
-    };
+    $http.get('/contactlist').success(function(response){
+        console.log("I got the data i requested");
+        
+        $scope.contactlist = response;
+    });
     
-     person2 = {
-        name: 'Emily',
-        email: 'Emily@email2.com',
-        number: '(222) 222-2222'
-    };
-    
-     person3 = {
-        name: 'John',
-        email: 'john@email3.com',
-        number: '(333) 333-3333'
-    };
-    
-    var contactlist = [person1,person2,person3];
-    $scope.contactlist = contactlist;
 });
--- server.js
+++ server.js
@@ -6,7 +6,31 @@ var app = express();
 //});
 
 app.use(express.static(__dirname + "/public"));
-
+app.get('/contactlist', function(req, res){
+    console.log("I received a GET request");
+    
+     person1 = {
+        name: 'Tim',
+        email: 'tim@email.com',
+        number: '(111) 111-1111'
+    };
+    
+     person2 = {
+        name: 'Emily',
+        email: 'Emily@email2.com',
+        number: '(222) 222-2222'
+    };
+    
+     person3 = {
+        name: 'John',
+        email: 'john@email3.com',
+        number: '(333) 333-3333'
+    };
+    
+    var contactlist = [person1,person2,person3];
+    
+    res.json(contactlist);
+});
 
 app.listen(3000);
 console.log("server running on port 3000");
