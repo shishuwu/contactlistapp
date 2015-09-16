c19f7f8f76c973761e5992d80f06da09e7840f92
Jason Shi <shuwu.shi@gmail.com>
2015-09-16 19:44:21 +0800

    Mongodb: npm install mongojs and user the api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 //app.get('/', function(req, res){
 //	res.send("Hello world from server.js");
 //});
@@ -9,27 +10,32 @@ app.use(express.static(__dirname + "/public"));
 app.get('/contactlist', function(req, res){
     console.log("I received a GET request");
     
-     person1 = {
-        name: 'Tim',
-        email: 'tim@email.com',
-        number: '(111) 111-1111'
-    };
+//     person1 = {
+//        name: 'Tim',
+//        email: 'tim@email.com',
+//        number: '(111) 111-1111'
+//    };
+//    
+//     person2 = {
+//        name: 'Emily',
+//        email: 'Emily@email2.com',
+//        number: '(222) 222-2222'
+//    };
+//    
+//     person3 = {
+//        name: 'John',
+//        email: 'john@email3.com',
+//        number: '(333) 333-3333'
+//    };
+//    
+//    var contactlist = [person1,person2,person3];
+//    
+//    res.json(contactlist);
     
-     person2 = {
-        name: 'Emily',
-        email: 'Emily@email2.com',
-        number: '(222) 222-2222'
-    };
-    
-     person3 = {
-        name: 'John',
-        email: 'john@email3.com',
-        number: '(333) 333-3333'
-    };
-    
-    var contactlist = [person1,person2,person3];
-    
-    res.json(contactlist);
+    db.contactlist.find(function(err, docs){
+        console.log(docs);
+        res.json(docs);
+    });
 });
 
 app.listen(3000);
