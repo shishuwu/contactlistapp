a63ed8e9e69fa440d447c15c0c8c28ee75d50509
Jason Shi <shuwu.shi@gmail.com>
2015-09-16 22:10:07 +0800

    Add: add contact call-back response and refresh function

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -8,15 +8,23 @@ var app = angular.module('app', []);
 app.controller('AppCtrl', function ($scope, $http) {
     console.log("Hello world from controller.");
     
-    $http.get('/contactlist').success(function(response){
-        console.log("I got the data i requested");
-        
-        $scope.contactlist = response;
-    });
-   
+    var refresh = function(){
+        $http.get('/contactlist').success(function(response){
+            console.log("I got the data i requested");
+
+            $scope.contactlist = response;
+            $scope.contact = "";
+        });
+    }
+    
+    refresh();
     
     $scope.addContact = function(){
         console.log($scope.contact);
-        $http.post('/contactlist', $scope.contact);
+        $http.post('/contactlist', $scope.contact).success(function(response){
+            console.log(response);
+            refresh();
+        });
     }
+
 });
